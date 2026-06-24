df938ceab552c1e085ed29495c5074524cea51ad
Wizard <wizard@coderwizard.local>
2026-06-24 13:39:12 +0530

    update the frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: false,
 });
 
 export const postTransaction = (data) => API.post("/transaction", data);
